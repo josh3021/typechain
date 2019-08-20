23dcc4234bbeaf5933aef1627e64bcb2bb56c695
josh3021 <joseonghwan3021@gmail.com>
2019-08-20 15:54:39 +0900

    #9 Validating Block Structure(final)

--- dist/index.js
+++ dist/index.js
@@ -9,22 +9,52 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Crypto = __importStar(require("crypto-js"));
 class Block {
-    constructor(index, hash, prevHash, data, timestamp) {
+    constructor(index, hash, previousHash, data, timestamp) {
         this.index = index;
         this.hash = hash;
-        this.prevHash = prevHash;
+        this.previousHash = previousHash;
         this.data = data;
         this.timestamp = timestamp;
     }
 }
-Block.calculateBlockHash = (index, prevHash, data, timestamp) => Crypto.SHA256(index + prevHash + data + timestamp).toString();
+Block.calculateBlockHash = (index, previousHash, data, timestamp) => Crypto.SHA256(index + previousHash + data + timestamp).toString();
+Block.validateStructure = (anyBlock) => typeof anyBlock.index === "number" &&
+    typeof anyBlock.hash === "string" &&
+    typeof anyBlock.previousHash === "string" &&
+    typeof anyBlock.data === "string" &&
+    typeof anyBlock.timestamp === "number";
 const genesisBlock = new Block(0, "kindOfHash", "", "SuperDummyData", 1111111);
 Object.freeze(genesisBlock);
 const blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const latestBlock = getLatestBlock();
+    const newIndex = latestBlock.index + 1;
+    const previousHash = latestBlock.hash;
+    const newTimestamp = getTimestamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousHash, data, newTimestamp);
+    const newBlock = new Block(newIndex, newHash, previousHash, data, newTimestamp);
+    Object.freeze(newBlock);
+    addBlockToChain(newBlock);
+    return newBlock;
+};
+const validateBlock = (latestBlock, newBlock) => Block.validateStructure(newBlock) &&
+    latestBlock.index + 1 === newBlock.index &&
+    latestBlock.hash === newBlock.previousHash &&
+    newBlock.hash === Block.calculateBlockHash(newBlock.index, newBlock.previousHash, newBlock.data, newBlock.timestamp) &&
+    Math.ceil(Math.log10(newBlock.timestamp + 1)) === 10 && // timestamp must be 10 digits
+    newBlock.timestamp > 0; // timestamp must not be minus value
+const addBlockToChain = (newBlock) => {
+    const latestBlock = getLatestBlock();
+    if (!validateBlock(latestBlock, newBlock)) {
+        return console.error('Error: Invalid Block!');
+    }
+    blockchain.push(newBlock);
+};
+createNewBlock("blabla");
 console.log(getBlockchain());
-console.log(getLatestBlock());
-console.log(getTimestamp());
+createNewBlock("blabla2");
+console.log(getBlockchain());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,30 @@ import * as Crypto from "crypto-js";
 class Block {
   public index: number;
   public hash: string;
-  public prevHash: string;
+  public previousHash: string;
   public data: string;
   public timestamp: number;
 
-  static calculateBlockHash = (index: number, prevHash: string, data: string, timestamp: number): string => Crypto.SHA256(index + prevHash + data + timestamp).toString();
+  static calculateBlockHash = (index: number, previousHash: string, data: string, timestamp: number): string =>
+    Crypto.SHA256(index + previousHash + data + timestamp).toString();
 
-  constructor (index: number, hash: string, prevHash: string, data: string, timestamp: number) {
+  static validateStructure = (anyBlock: Block): boolean =>
+    typeof anyBlock.index === "number" &&
+    typeof anyBlock.hash === "string" &&
+    typeof anyBlock.previousHash === "string" &&
+    typeof anyBlock.data === "string" &&
+    typeof anyBlock.timestamp === "number";
+  
+  constructor (index: number, hash: string, previousHash: string, data: string, timestamp: number) {
     this.index = index;
     this.hash = hash;
-    this.prevHash = prevHash;
+    this.previousHash = previousHash;
     this.data = data;
     this.timestamp = timestamp;
   }
 }
 
-const genesisBlock = new Block(0, "kindOfHash", "", "SuperDummyData", 1111111);
+const genesisBlock: Block = new Block(0, "kindOfHash", "", "SuperDummyData", 1111111);
 Object.freeze(genesisBlock);
 
 const blockchain: Block[] = [genesisBlock];
@@ -29,8 +37,42 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
+const createNewBlock = (data: string): Block => {
+  const latestBlock: Block = getLatestBlock();
+  const newIndex: number = latestBlock.index + 1;
+  const previousHash: string = latestBlock.hash;
+  const newTimestamp: number = getTimestamp();
+  const newHash = Block.calculateBlockHash(newIndex, previousHash, data, newTimestamp);
+
+  const newBlock: Block = new Block(newIndex, newHash, previousHash, data, newTimestamp);
+  Object.freeze(newBlock);
+
+  addBlockToChain(newBlock);
+  return newBlock;
+}
+
+const validateBlock = (latestBlock: Block, newBlock: Block): boolean =>
+  Block.validateStructure(newBlock) &&
+  latestBlock.index + 1 === newBlock.index &&
+  latestBlock.hash === newBlock.previousHash &&
+  newBlock.hash === Block.calculateBlockHash(newBlock.index, newBlock.previousHash, newBlock.data, newBlock.timestamp) &&
+  Math.ceil(Math.log10(newBlock.timestamp + 1)) === 10 && // timestamp must be 10 digits
+  newBlock.timestamp > 0 // timestamp must not be minus value
+
+const addBlockToChain = (newBlock: Block): void => {
+  const latestBlock: Block = getLatestBlock();
+
+  if (!validateBlock(latestBlock, newBlock)) {
+    return console.error('Error: Invalid Block!');
+  }
+
+  blockchain.push(newBlock);
+}
+
+createNewBlock("blabla");
 console.log(getBlockchain());
-console.log(getLatestBlock());
-console.log(getTimestamp());
+
+createNewBlock("blabla2");
+console.log(getBlockchain());
 
 export {};
