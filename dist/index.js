a1ea9595dd4424717602f90402592772e0235455
josh3021 <joseonghwan3021@gmail.com>
2019-08-20 12:06:55 +0900

    #4Types in Typescript

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "SeonghwanCho", age = 19, gender = "male";
+const sayHello = (name, age, gender) => {
+    console.log(`Hello! ${name}, you are ${age} years old, and you are ${gender}`);
+};
+sayHello(name, age);
+//# sourceMappingURL=index.js.map
