76fbe86e1efc04f9728a2e36c76e83c04cabd77c
josh3021 <joseonghwan3021@gmail.com>
2019-08-20 11:47:30 +0900

    #3 First steps with Typescript

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert('hi');
+"use strict";
+exports.__esModule = true;
+var name = "SeonghwanCho", age = 19, gender = "male";
+var sayHello = function (name, age, gender) {
+    console.log("Hello! " + name + ", you are " + age + " years old, and you are " + gender);
+};
+sayHello(name, age);
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-alert('hi');
+const name = "SeonghwanCho",
+  age = 19,
+  gender = "male";
+
+const sayHello = (name: any, age: any, gender?: any) => {
+  console.log(`Hello! ${name}, you are ${age} years old, and you are ${gender}`);
+}
+
+sayHello(name, age);
+
+export {};
